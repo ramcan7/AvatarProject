9f54e23cae1c7678f25d9b3cff9f58463f9c4ddf
ramcan7 <m.ramos@pucp.edu.pe>
2025-10-08 21:20:49 -0500

    Base movement done

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 moveInput;
+    [SerializeField]
+    private float speed = 5f;
+    private Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMove(InputValue value) {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate() {
+        rb.linearVelocity = moveInput * speed;
+    }
+}
